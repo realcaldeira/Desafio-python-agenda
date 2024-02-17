0ae9d1ed7dd121c90571a263c089c8674d7835da
Lucas Caldeira <lucas.costa@aggrandize.com.br>
2024-02-16 23:01:00 -0300

    feat: adicionando codigo completo

--- agenda.py
+++ agenda.py
@@ -1,8 +1,25 @@
 
 def adicionar_contato(nome, telefone, email):
-  data.append({"nome": nome, "telefone": telefone, "email": email,"favorito": False})
+  data.append({"nome": nome, "telefone": telefone, "email": email, "favorito": False})
   print("Contato adicionado com sucesso!")
   return
+
+def visualizar_contatos(contato):
+  for indice,contato in enumerate(data, start=1):
+    print(f"{indice}. {contato['favorito']} - {contato['nome']} - {contato['telefone']} - {contato['email']}")
+  return
+
+def atualizar_contato(data, numero, nome, telefone, email):
+    if int(numero) <= len(data):
+        data[int(numero)-1] = {"nome": nome, "telefone": telefone, "email": email, "favorito": False}
+        print("Contato atualizado com sucesso!")
+    return
+
+def marcar_contato(data, numero):
+    if int(numero) <= len(data):
+        data[int(numero)-1]["favorito"] = True
+        print("Contato marcado com sucesso!")
+    return
 
 data = []
 
@@ -23,6 +40,29 @@ while True:
     email = input("Digite o email do contato: ")
     adicionar_contato(nome, telefone, email)
   elif opcao == "2":
-    print(data)
+    visualizar_contatos(data)
+  elif opcao == "3":
+    numero = input("Digite o id do contato: ")
+    nome = input("Digite o nome do contato: ")
+    telefone = input("Digite o telefone do contato: ")
+    email = input("Digite o email do contato: ")
+    atualizar_contato(data, numero, nome, telefone, email)
+  elif opcao == "4":
+    numero = input("Digite o id do contato: ")
+    marcar_contato(data, numero)
+  elif opcao == "5":
+    numero = input("Digite o id do contato: ")
+    print(data[int(numero)-1])
+  elif opcao == "6":
+    for contato in data:
+        if contato["favorito"]:
+            print(contato)
+  elif opcao == "7":
+    numero = input("Digite o id do contato: ")
+    if int(numero) <= len(data):
+        data.pop(int(numero)-1)
+        print("Contato deletado com sucesso!")
+    else:
+        print("Contato não encontrado!")
   else:
     print("Opção inválida")
